4148f282d73b9774ee53180ed0b8a6b114ef7490
gumends <gustavo08.mendes@gmail.com>
2025-01-01 23:42:03 -0300

    Click para ver detalhes do filme

--- src/app/components/Carrosel.tsx
+++ src/app/components/Carrosel.tsx
@@ -4,6 +4,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { Popular } from '@/types/popular.type';
+import { useRouter } from "next/navigation";
 
 function CenterMode({ filmes }: { filmes: Popular[] }) {
   const settings = {
@@ -15,16 +16,20 @@ function CenterMode({ filmes }: { filmes: Popular[] }) {
     slidesToShow: 3,
     speed: 500,
   };
+
+  const router = useRouter();
   
   return (
     <div className="slider-container">
       <Slider {...settings}>
         {filmes.map((filme) => (
-          <div key={filme.id}>
-          <Box key={filme.id} sx={{ mx: 2 }}>
+          <Box 
+          key={filme.id} 
+          sx={{ mx: 2, p: 2.5, cursor: 'pointer', transition: 'transform 0.3s ease-in-out', '&:hover': { transform: 'scale(1.1)' }}}
+          onClick={() => { router.push(`/filmes/detalhes?id=${filme.id}`) }}
+          >
             <img src={`https://image.tmdb.org/t/p/w500/${filme.poster_path}`} alt={filme.title} />
           </Box>
-          </div>
         ))}
       </Slider>
     </div>
