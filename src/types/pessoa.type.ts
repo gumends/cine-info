c937c38604b4229c7103a8dab5fea757f348782f
gumends <gustavo08.mendes@gmail.com>
2025-01-12 23:50:27 -0300

    Type de pessoa

---
+++ src/types/pessoa.type.ts
@@ -0,0 +1,10 @@
+export interface IPessoa {
+    adult: boolean
+    gender: number
+    id: number
+    known_for_department: string
+    name: string
+    original_name: string
+    popularity: number
+    profile_path: string
+}
