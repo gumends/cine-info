7600ff845656c9f8324b180ba878c3680f89a76e
gumends <gustavo08.mendes@gmail.com>
2025-01-14 23:48:52 -0300

    Ajustes no Content

--- src/app/components/Content.tsx
+++ src/app/components/Content.tsx
@@ -6,7 +6,7 @@ import AppAppBar from './AppAppBar';
 import Footer from './Footer';
 import getMPTheme from '../theme/getMPTheme';
 import TemplateFrame from '../TemplateFrame';
-import { Box, Container } from '@mui/material';
+import { Box } from '@mui/material';
 
 interface IMarketingPageProps {
   children: React.ReactNode;
@@ -47,37 +47,42 @@ export default function MarketingPage({
       >
         <Box
           id="hero"
-          sx={(theme) => ({
+          sx={{
             width: '100%',
             minHeight: '100vh',
             backgroundRepeat: 'no-repeat',
             backgroundAttachment: 'fixed',
             backgroundImage: `
-              radial-gradient(circle at 30% 20%, rgba(70, 130, 180, 0.6), transparent 50%),
-              radial-gradient(circle at 70% 50%, rgba(75, 0, 130, 0.5), transparent 60%),
-              radial-gradient(circle at 50% 80%, rgba(123, 104, 238, 0.4), transparent 70%)
-            `,
+      radial-gradient(circle at 30% 20%, rgba(70, 130, 180, 0.6), transparent 50%),
+      radial-gradient(circle at 70% 50%, rgba(75, 0, 130, 0.5), transparent 60%),
+      radial-gradient(circle at 50% 80%, rgba(123, 104, 238, 0.4), transparent 70%)
+    `,
             backgroundSize: 'cover',
             backgroundPosition: 'center',
-            ...theme.applyStyles('dark', {
-              backgroundImage: `
-                radial-gradient(circle at 30% 20%, rgba(70, 130, 180, 0.6), transparent 50%),
-                radial-gradient(circle at 70% 50%, rgba(75, 0, 130, 0.5), transparent 60%),
-                radial-gradient(circle at 50% 80%, rgba(123, 104, 238, 0.4), transparent 70%)
-              `,
-            }),
-          })}
+          }}
         >
           <CssBaseline enableColorScheme />
-          <Container
+          {/* AppAppBar sempre no topo */}
+          <Box
             sx={{
-              pt: 5,
-              pb: { xs: 8, sm: 12 },
+              position: 'relative', // Fixo no topo
+              top: 20,
+              left: 0,
+              width: '100%',
+              zIndex: (theme) => theme.zIndex.drawer + 1, // Prioridade alta no zIndex
             }}
           >
             <AppAppBar />
+          </Box>
+          {/* Conteúdo abaixo do AppAppBar */}
+          <Box
+            sx={{
+              paddingTop: '64px', // Espaço para o AppAppBar (ajuste conforme sua altura)
+              zIndex: 1,
+            }}
+          >
             {children}
-          </Container>
+          </Box>
         </Box>
         <Footer />
       </ThemeProvider>
