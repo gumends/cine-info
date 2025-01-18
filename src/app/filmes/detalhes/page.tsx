532934acec165e086da9a422d2142d74895f8cdb
gumends <gustavo08.mendes@gmail.com>
2025-01-17 23:43:57 -0300

    Reajustados os generos de filmes

--- src/app/filmes/detalhes/page.tsx
+++ src/app/filmes/detalhes/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { Box, Card, CardContent, CardMedia, Divider, Typography } from '@mui/material';
+import { Box, Card, CardContent, CardMedia, Chip, Container, Divider, Typography } from '@mui/material';
 import Content from '../../components/Content';
 import { useEffect, useState } from 'react';
 import * as films from '@/services/films.service';
@@ -34,6 +34,8 @@ const Home: React.FC = () => {
         if (movieId) {
             films.getFilme(Number(movieId))
                 .then((data: IFilme) => {
+                    console.log(data);
+
                     setFilme(data);
                     setPoster_path(data.poster_path);
                 })
@@ -50,7 +52,7 @@ const Home: React.FC = () => {
 
             films.getServices(Number(movieId))
                 .then((data: IService) => {
-                    console.log(data);
+                    // console.log(data);
                 })
         }
     }, []);
@@ -71,95 +73,104 @@ const Home: React.FC = () => {
         //rgbaDark={colors[1] ? colors[1] : "#000000"}
         //rgbaLight={colors[2] ? colors[2] : "#000000"}
         >
-            <Stack
-                sx={{
-                    width: "100%",
-                    justifyContent: "space-between",
-                    my: 8,
-                    gap: 2,
-                }}
-                direction={{ xs: "column", sm: "row", md: "row" }}
-                alignItems={{ xs: "center", sm: "flex-start", md: "flex-start" }}
-                marginTop={{ xs: 15, sm: 0, md: 0 }}
-            >
-                <Box>
-                    <Typography variant="h4" >{filme?.title}</Typography>
-                    <Typography variant="body1" sx={{ mt: 4, width: { xs: "100%", sm: "70%", md: "70%" } }}>{filme?.overview}</Typography>
-
-                </Box>
-                <Box>
-                    <CardMedia
-                        component="img"
-                        sx={{ minWidth: 300, maxWidth: "100%", height: 500 }}
-                        image={`https://image.tmdb.org/t/p/original${filme?.poster_path}`}
-                        alt={filme?.title}
-                    />
-                    <Box sx={{ display: "flex", mt: 4, gap: 1, alignItems: "center" }}>
-                        {filme?.genres.map((genre, key) => (
-                            <Box key={key} sx={{ display: "flex", alignItems: "center", gap: 1 }}><CircleIcon sx={{ width: 8, height: 8 }} /> <Typography variant="body2" >{genre.name}</Typography></Box>
-                        ))}
+            <Container>
+                <Stack
+                    sx={{
+                        width: "100%",
+                        height: 500,
+                        justifyContent: "space-between",
+                        my: 8,
+                        gap: 2,
+                    }}
+                    direction={{ xs: "column", sm: "row", md: "row" }}
+                    alignItems={{ xs: "center", sm: "flex-start", md: "flex-start" }}
+                    marginTop={{ xs: 15, sm: 0, md: 0 }}
+                >
+                    <Box sx={{ width: "100%", display: "flex", flexDirection: "column", justifyContent: "space-between", height: "100%" }}>
+                        <Box>
+                            <Typography variant="h4" sx={{ fontSize: 30, width: "100%", fontWeight: "bold" }} >{filme?.title}</Typography>
+                            <Typography variant="body1" sx={{ mt: 4, fontSize: 20, width: '70%', color: "rgba(255, 255, 255, 0.8)" }}>{filme?.overview}</Typography>
+                        </Box>
+                        <Box sx={{ display: "flex", mt: 4, gap: 1, alignItems: "center" }}>
+                            {filme?.genres.map((genre, key) => (
+                                <Box key={key} sx={{ display: "flex", border: "1px solid rgba(255, 255, 255, 0.1)", alignItems: "center", gap: 1, bgcolor: "rgba(255, 255, 255, 0.1)", borderRadius: 1, p: 1 }}>
+                                    <Typography variant="body2" >
+                                        {genre.name}
+                                    </Typography>
+                                </Box>
+                            ))}
+                        </Box>
                     </Box>
+                    <Box>
+                        <CardMedia
+                            component="img"
+                            sx={{ minWidth: 300, maxWidth: "100%", height: 500, border: "1px solid rgba(255, 255, 255, 0.1)", borderRadius: 1 }}
+                            image={`https://image.tmdb.org/t/p/original${filme?.poster_path}`}
+                            alt={filme?.title}
+                        />
+
+                    </Box>
+                </Stack>
+                <Typography sx={{ fontSize: 20 }} gutterBottom variant="h5" component="div">
+                    Elenco
+                </Typography>
+                <Divider />
+                <Box sx={{ maxWidth: "100%", display: "flex", overflowX: "auto", gap: 2, height: "350px", mt: 5 }}>
+                    <Flicking
+                        align="prev"
+                        circular={false}
+                    >
+                        {credts.map((elenco, key) => (
+                            <Card key={key} sx={{ minWidth: 275, mx: 1 }}>
+                                <CardMedia
+                                    sx={{ height: 240 }}
+                                    image={`https://image.tmdb.org/t/p/w500${elenco.profile_path}`}
+                                    title={elenco.name}
+                                />
+                                <CardContent>
+                                    <Typography sx={{ fontSize: 20, mt: 1 }} gutterBottom variant="h5" component="div">
+                                        {elenco.name}
+                                    </Typography>
+                                    <Typography sx={{ fontSize: 12 }} gutterBottom variant="body2" component="div">
+                                        {elenco.character}
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                        ))}
+                    </Flicking>
                 </Box>
-            </Stack>
-            <Typography sx={{ fontSize: 20 }} gutterBottom variant="h5" component="div">
-                Elenco
-            </Typography>
-            <Divider />
-            <Box sx={{ maxWidth: "100%", display: "flex", overflowX: "auto", gap: 2, height: "350px", mt: 5 }}>
-                <Flicking
-                    align="prev"
-                    circular={false}
-                >
-                    {credts.map((elenco, key) => (
-                        <Card key={key} sx={{ minWidth: 275, mx: 1 }}>
-                            <CardMedia
-                                sx={{ height: 240 }}
-                                image={`https://image.tmdb.org/t/p/w500${elenco.profile_path}`}
-                                title={elenco.name}
-                            />
-                            <CardContent>
-                                <Typography sx={{ fontSize: 20, mt: 1 }} gutterBottom variant="h5" component="div">
-                                    {elenco.name}
-                                </Typography>
-                                <Typography sx={{ fontSize: 12 }} gutterBottom variant="body2" component="div">
-                                    {elenco.character}
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                    ))}
-                </Flicking>
-            </Box>
-            <Typography sx={{ fontSize: 20, mt: 4 }} gutterBottom variant="h5" component="div">
-                Trailers
-            </Typography>
-            <Divider />
-            <Box
-                sx={{
-                    minWidth: "100%",
-                    height: { xs: 300, sm: 400, md: 500 },
-                    display: "flex",
-                    overflowX: "auto",
-                    gap: 2,
-                    mt: 5,
-                    overflow: "auto"
-                }}>
-                <Flicking
-                    align="prev"
-                    circular={true}
-                >
-                    {videos.map((video, key) => (
-                        <Card key={key} sx={{ width: "100%", height: "100%" }}>
-                            <CardMedia
-                                component="iframe"
-                                sx={{ width: '100%', height: "100%" }}
-                                src={`https://www.youtube.com/embed/${video.key}?vq=hd1080`}
-                                allow="autoplay; encrypted-media"
-                                title="Trailer do filme"
-                            />
-                        </Card>
-                    ))}
-                </Flicking>
-            </Box>
+                <Typography sx={{ fontSize: 20, mt: 4 }} gutterBottom variant="h5" component="div">
+                    Trailers
+                </Typography>
+                <Divider />
+                <Box
+                    sx={{
+                        minWidth: "100%",
+                        height: { xs: 300, sm: 400, md: 500 },
+                        display: "flex",
+                        overflowX: "auto",
+                        gap: 2,
+                        mt: 5,
+                        overflow: "auto"
+                    }}>
+                    <Flicking
+                        align="prev"
+                        circular={true}
+                    >
+                        {videos.map((video, key) => (
+                            <Card key={key} sx={{ width: "100%", height: "100%" }}>
+                                <CardMedia
+                                    component="iframe"
+                                    sx={{ width: '100%', height: "100%" }}
+                                    src={`https://www.youtube.com/embed/${video.key}?vq=hd1080`}
+                                    allow="autoplay; encrypted-media"
+                                    title="Trailer do filme"
+                                />
+                            </Card>
+                        ))}
+                    </Flicking>
+                </Box>
+            </Container>
         </Content>
     );
 }
