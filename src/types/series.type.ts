56f671ed95f1b93a48a5942fc5f2b54ef001e09c
gumends <gustavo08.mendes@gmail.com>
2025-01-19 23:49:31 -0300

    Intenfaces de series

---
+++ src/types/series.type.ts
@@ -0,0 +1,70 @@
+interface IGenrt {
+  id: number;
+  name: string;
+}
+
+interface IReleaseDates {
+  results: {
+    iso_3166_1: string;
+    release_dates: {
+      certification: string;
+      descriptors: [];
+      iso_639_1: string;
+      note: string;
+      release_date: string;
+      type: number;
+    }[];
+  }[];
+}
+
+interface Ilast_episode_to_air {
+  air_date: string;
+  episode_number: number;
+  episode_type: string;
+  id: number;
+  name: string;
+  overview: string;
+  production_code: string;
+  runtime: number;
+  season_number: number;
+  show_id: number;
+  still_path: string;
+  vote_average: number;
+  vote_count: number;
+}
+
+interface ISesson {
+  air_date: string;
+  episode_count: number;
+  id: number;
+  name: string;
+  overview: string;
+  poster_path: string;
+  season_number: number;
+  vote_average: number;
+}
+
+export interface ISerie {
+  adult: boolean;
+  backdrop_path: string;
+  genres: IGenrt[];
+  homepage: string;
+  first_air_date: string;
+  id: number;
+  in_production: boolean;
+  last_air_date: string;
+  last_episode_to_air: Ilast_episode_to_air;
+  name: string;
+  next_episode_to_air: Ilast_episode_to_air;
+  number_of_episodes: number;
+  number_of_seasons: number;
+  original_language: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  sessons: ISesson[];
+  release_date: string;
+  // release_dates: IReleaseDates;
+  vote_average: number;
+  vote_count: number;
+}
